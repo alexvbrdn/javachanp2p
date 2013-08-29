85ae4fc25d24f46879675f0ede136aa9a0b9419a
KiLlOrBe <alexandre.vanbeurden78@gmail.com>
2013-08-29 13:38:21 +0200

    Ajout des interactions des boutons

--- Javachanp2p/src/affichage/Fenetre.java
+++ Javachanp2p/src/affichage/Fenetre.java
@@ -1,10 +1,13 @@
 package affichage;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
  
-public class Fenetre extends JFrame{
+public class Fenetre extends JFrame implements ActionListener{
 	private JPanel pan = new JPanel();
 	private JButton boutonCreer = new JButton("Créer une salle");
 	private JButton boutonRejoindre = new JButton("Rejoindre une salle");
@@ -14,9 +17,26 @@ public class Fenetre extends JFrame{
 		this.setSize(300, 150);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
+		
+		boutonCreer.addActionListener(this);
+		boutonRejoindre.addActionListener(this);
 		pan.add(boutonCreer);
 		pan.add(boutonRejoindre);
 		this.setContentPane(pan);
 		this.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Object obj = arg0.getSource();
+		if(obj.equals(boutonCreer)){
+			//Créer une salle
+			System.out.println("Créer une salle");
+		}
+		else{
+			//Rejoindre une salle
+			System.out.println("Rejoindre une salle");
+		}
+		
 	}      
 }
