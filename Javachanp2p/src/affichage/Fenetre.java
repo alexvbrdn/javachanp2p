b22e940291441baa0dcd344446c16fb6f1dc63a1
KiLlOrBe <alexandre.vanbeurden78@gmail.com>
2013-08-29 13:30:03 +0200

    Ajout des boutons dans la fenêtre

--- Javachanp2p/src/affichage/Fenetre.java
+++ Javachanp2p/src/affichage/Fenetre.java
@@ -1,24 +1,22 @@
 package affichage;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-
-
-
-public class Fenetre {
+import javax.swing.JPanel;
+ 
+public class Fenetre extends JFrame{
+	private JPanel pan = new JPanel();
+	private JButton boutonCreer = new JButton("Créer une salle");
+	private JButton boutonRejoindre = new JButton("Rejoindre une salle");
+ 
 	public Fenetre(){
-		JFrame fenetre = new JFrame();
-        
-	    //Définit un titre pour notre fenêtre
-	    fenetre.setTitle("Chat p2p");
-	    //Définit sa taille : 400 pixels de large et 100 pixels de haut
-	    fenetre.setSize(400, 100);
-	    //Nous demandons maintenant à notre objet de se positionner au centre
-	    fenetre.setLocationRelativeTo(null);
-	    //Termine le processus lorsqu'on clique sur la croix rouge
-	    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    //Et enfin, la rendre visible        
-	    fenetre.setVisible(true);
-	    
-	    
-	}
+		this.setTitle("Chat P2P");
+		this.setSize(300, 150);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		pan.add(boutonCreer);
+		pan.add(boutonRejoindre);
+		this.setContentPane(pan);
+		this.setVisible(true);
+	}      
 }
