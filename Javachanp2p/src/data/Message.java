0ec4b11207f57582c44b8ab696553fd567961c6f
ogrergo <louis.vanbeurden@gmail.com>
2013-09-20 14:16:40 +0200

    Ajout des types de messages et du codage des paquets

--- Javachanp2p/src/data/Message.java
+++ Javachanp2p/src/data/Message.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 
 import javax.imageio.ImageIO;
@@ -12,19 +13,26 @@ import javax.imageio.ImageIO;
 public class Message {
 	public Message(String str){
 		text = str;
-		haveAImage = false;
+		type = TYPE_MESSAGE_TEXT;
 	}
 	
 	public Message(String str, BufferedImage img){
 		text = str;
 		image = img;
-		haveAImage = true;
+		type = str.equals("") ? TYPE_MESSAGE_IMAGE : TYPE_MESSAGE_TEXT_IMAGE;
 	}
+	
+	/**
+	 * Differents type de message possible : à deplacer ou il y aurat toute les types de communications
+	 */
+	public static final byte TYPE_MESSAGE_TEXT = 0;
+	public static final byte TYPE_MESSAGE_TEXT_IMAGE = 1;
+	public static final byte TYPE_MESSAGE_IMAGE = 2;
 	
 	private Date date;
 	private String text;
 	private BufferedImage image;
-	private boolean haveAImage;
+	private byte type;
 	private long ID = 0;
 	
 	public void genereDate(){
@@ -43,31 +51,39 @@ public class Message {
 		this.ID = ID;
 	}
 	
+	
 	public boolean aUneImage(){
-		return haveAImage;
+		return type == TYPE_MESSAGE_IMAGE || type == TYPE_MESSAGE_TEXT_IMAGE;
 	}
 	
-	//pour l'émission
-	public InputStream aEnvoyer(){
-		String packet = "";
-		packet = packet + "<message>";
-		{
-			packet = packet + "<text>";
-			packet = packet + text;
-			packet = packet + "</text>";
+	/**
+	 * encodage :
+	 * HEADER:
+	 *	1er octet : TYPE_MESSAGE
+	 *	SI text alors :
+	 *			2 prochains octets taille du message
+	 *	SI IMAGE alors :
+	 *			3 prochains octets taille de l'image
+	 * BODY :
+	 * 	TEXT
+	 * 	IMAGE 	
+	 * @return
+	 */
+	public InputStream getBytes(){ //implementation du text seul
+		int taille_text = text.length();
+		
+		byte taille_text_1 = (byte) (text.length() % 256);
+		byte taille_text_2 = (byte) (text.length() / 256);
+		
+		byte[] buffer = new byte[taille_text + 3];
+		buffer[0] = type;
+		buffer[1] = taille_text_2;
+		buffer[2] = taille_text_1;
+		
+		for( int i = 0 ; i < taille_text; i++){
+			buffer[i + 3] = (byte) text.charAt(i);
 		}
-		{
-			packet = packet + "<date>";
-			packet = packet + date;
-			packet = packet + "</date>";
-		}
-		{
-			packet = packet + "<image>";
-			packet = packet + haveAImage;
-			packet = packet + "</image>";
-		}
-		packet = packet + "</message>";
-		return new ByteArrayInputStream(packet.getBytes());
+		return new ByteArrayInputStream( buffer );
 	}
  
 	public InputStream getImageInputStream(){
